b5ea0fb8a18d88b1f81809a7b8cf925b6e76f64c
Jackie14 <13701673173@163.com>
2018-02-27 11:12:45 +0800

    Add method to print chess borad

--- eight_queen.py
+++ eight_queen.py
@@ -2,7 +2,25 @@
 
 # This script it used to resolve the eight queen problem
 
+def print_chess_board():
+    """
+    print the chess board
+    chess board is a 8X8 grid
+    """
+    #Print horizontal axis numbers: 1 2 3 4 5 6 7 8
+    print(" ", end="")
+    for i in range(1, 9):
+        print(" %d "%i, end="")
+    print()
+
+    #Print the matrix
+    for i in range(8):
+        print(i+1, end="")
+        for j in range(8):
+            print("[ ]",end="")
+        print("")
+
 
 if __name__ == "__main__" :
-
+    print_chess_board()
     print("Failed to resolve Eight Queen problem")
